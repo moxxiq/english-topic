f2b2ca090af3082be87a2401068f4582f6ef85fe
Moxxiq <dimanavsisto@gmail.com>
2021-06-22 23:02:44 +0300

    Retrieve id of the topic and only then information about it

--- web/topics/views.py
+++ web/topics/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.db.models import Count, Q
+from django.db.models import Count, Q, F
 from .models import Topic, Vote
 
 
 def index(request):
-    upvotes = Count('vote_value', filter=Q(vote_value=Vote.VoteEnum.UPVOTE))
-    downvotes = Count('vote_value', filter=Q(vote_value=Vote.VoteEnum.DOWNVOTE))
-    topic_top = Vote.objects.values('topic_id', 'topic__topic_text').annotate(
+    upvotes = Count(F('vote_value'), filter=Q(vote_value=Vote.VoteEnum.UPVOTE))
+    downvotes = Count(F('vote_value'), filter=Q(vote_value=Vote.VoteEnum.DOWNVOTE))
+    topic_top = Vote.objects.values('topic_id').annotate(
         upvotes=upvotes,
         downvotes=downvotes,
         offset=upvotes - downvotes,
     ).order_by('-offset')
-    topics_page = '<br>'.join((f"{t['topic__topic_text']} U:{t['upvotes']} D:{t['downvotes']}" for t in topic_top))
+    topic_top_dict = {t['topic_id']: {"upvotes": t['upvotes'], "downvotes": t['downvotes']} for t in topic_top}
+    topics_details = Topic.objects.filter(id__in=topic_top_dict.keys()).values()
+    topics_details_dict = {t['id']: {"text": t["topic_text"]} for t in topics_details}
+    topics_page = '<br>'.join(f"{topics_details_dict[i]['text']} Upvotes:{topic_top_dict[i]['upvotes']} Downvotes:{topic_top_dict[i]['downvotes']}" for i in topic_top_dict.keys())
     return HttpResponse(topics_page)
 
 
