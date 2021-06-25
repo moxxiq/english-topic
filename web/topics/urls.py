ebd8907d8a9cb5798c44413eb37c4bac93450b6f
Moxxiq <dimanavsisto@gmail.com>
2021-06-26 00:29:28 +0300

    Add a template system and depending files

--- web/topics/urls.py
+++ web/topics/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'topics'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:topic_id>/', views.topic, name='topic')
--- web/topics/views.py
+++ web/topics/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.template.defaulttags import register
+
 from django.db.models import Count, Q, F
 from .models import Topic, Vote
+
+
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key)
 
 
 def index(request):
@@ -12,11 +20,17 @@ def index(request):
         downvotes=downvotes,
         offset=upvotes - downvotes,
     ).order_by('-offset')
-    topic_top_dict = {t['topic_id']: {"upvotes": t['upvotes'], "downvotes": t['downvotes']} for t in topic_top}
-    topics_details = Topic.objects.filter(id__in=topic_top_dict.keys()).values()
-    topics_details_dict = {t['id']: {"text": t["topic_text"]} for t in topics_details}
-    topics_page = '<br>'.join(f"{topics_details_dict[i]['text']} Upvotes:{topic_top_dict[i]['upvotes']} Downvotes:{topic_top_dict[i]['downvotes']}" for i in topic_top_dict.keys())
-    return HttpResponse(topics_page)
+    topic_rating = {t['topic_id']: t['offset'] for t in topic_top}
+    topics_details = Topic.objects.filter(id__in=topic_rating.keys()).values()
+    topics_text = {t['id']: t["topic_text"] for t in topics_details}
+
+    template = loader.get_template('topics/index.html')
+    context = {
+        'top': topic_rating,
+        'texts': topics_text,
+    }
+    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 
 def topic(request, topic_id):
